ecd6e90e8dd05f94e1528a65483a0b9387229810
John Bond <github@johnbond.org>
2021-01-18 00:07:10 +0100

    update path

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ import sys
 import logging
 sys.path.insert(0, '/srv/http/nel.johnbond.org/')
 from webapp import app as application
-logging.basicConfig(filename='/var/log/apache2/nel.johnbond.org_flask.log', level=logging.INFO)
+logging.basicConfig(filename='/srv/http/nel.johnbond.org/app.log', level=logging.INFO)
 application.secret_key = 'a secret'

c546667954431213680102688b4528ccb3bcf88c
John Bond <github@johnbond.org>
2021-01-17 23:55:11 +0100

    add logging file

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import logging
 sys.path.insert(0, '/srv/http/nel.johnbond.org/')
 from webapp import app as application
+logging.basicConfig(filename='/srv/http/nel.johnbond.org//app.log', level=logging.INFO)
 application.secret_key = 'a secret'

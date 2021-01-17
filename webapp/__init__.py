f107d0a99f8afab0b5d083d628f472adea367379
John Bond <github@johnbond.org>
2021-01-17 23:52:21 +0100

    -

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -12,7 +12,7 @@ def root():
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
     app.logger.info(request.json)
-    return request.json
+    return str(request.json)
 
 if __name__ == "__main__":
     app.run()

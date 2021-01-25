8e65b0a2ce53e0ae2fbe88571893f377f77ae843
John Bond <github@johnbond.org>
2021-01-25 11:48:17 +0100

    can hav multiple pakets

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -13,9 +13,10 @@ def root():
 
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
-    nel_packet = request.json
-    nel_packet['client_ip'] = request.remote_addr
-    app.logger.info(json.dumps(nel_packet, indent=2, sort_keys=True))
+    nel_packets = request.json
+    for pkt in nel_packets:
+        pkt['client_ip'] = request.remote_addr
+    app.logger.info(json.dumps(nel_packets, indent=2, sort_keys=True))
     return str(request.json)
 
 if __name__ == "__main__":
